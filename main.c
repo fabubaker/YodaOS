9173028edebe98b96bab079b18968559ec05acb1
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2017-11-21 23:32:46 +0300

    clean up

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 void ProgA(void);
 void ProgB(void);
 void ProgC(void);
+
 struct TCB
 {
     struct TCB *next;
@@ -24,6 +25,7 @@ struct TCB
     void (*InitPC)(void);
     unsigned int PCR;
 };
+
 extern struct TCB TCBs[3] = {
         {&TCBs[1],&TCBs[0].R0,1,{0},0,0,0,0,0,0,ProgA,0x61000000},
         {&TCBs[2],&TCBs[1].R0,1,{0},0,0,0,0,0,0,ProgB,0x61000000},
@@ -34,8 +36,8 @@ unsigned int static Timervalue = 0;
 unsigned int static old = 0;
 unsigned int static done = 0;
 extern volatile struct TCB* RunPtr = &TCBs[0];
-extern volatile int* StackPtr=0;// = TCBs[0].StactPt;
-volatile char bootstarping;
+extern volatile int* StackPtr=0;
+volatile char bootstrapping;
 
 void Timer2AHandler()
 {
@@ -43,50 +45,32 @@ void Timer2AHandler()
     asm("   .global RunPtr");
     asm("StackAdd: .field RunPtr");
 
-    if(bootstarping == 1)
+    if(bootstrapping == 1)
     {
-        bootstarping = 0;
-        //startup();
+        bootstrapping = 0;
         asm("  LDR R0,StackAdd ");
         asm("  LDR R0,[R0] ");
         asm("  LDR R13,[R0,#4]");
-        //asm("  BX LR");
-        //asm("  LDR R13,[R0]");
     }
     else
     {
-        // save stack pointer
-        //StackPtr = &RunPtr->StactPt;
         asm("    LDR R0,StackAdd");
-        asm("  LDR R0,[R0] ");
+        asm("    LDR R0,[R0] ");
         asm("    STR R13,[R0,#4]");
-        // load the new task
         RunPtr = RunPtr->next;
         asm("  LDR R0,StackAdd ");
         asm("  LDR R0,[R0] ");
         asm("  LDR R13,[R0,#4]");
-        //asm("  BX LR");
-
-        //StackPtr = RunPtr->StactPt;
-        // now load the stack pointer from this new task
-        //asm("    LDR R0,StackAdd");
-        //asm("    LDR R13,[R0]");
-        //startup();
-
     }
 
 }
+
 void Timer2AInit()
 {
     SYSCTL_RCGCTIMER_R |= 0x04;
-    //GPIO_PORTB_DIR_R = 0;
-    //GPIO_PORTB_AFSEL_R = 0x40;
-    //GPIO_PORTB_DEN_R = 0x40;
-    //GPIO_PORTB_PCTL_R = (GPIO_PORTB_PCTL_R &0xF0FFFFFF)+0x07000000;
-    TIMER2_CTL_R &= 0x00000000;    // disable timer 0
+    TIMER2_CTL_R &= 0x00000000;    
     TIMER2_CFG_R = 0x00000000;
     TIMER2_TAMR_R = 0x00000002;
-    //TIMER2_CTL_R |= 0x0000000C;
     TIMER2_TAILR_R = 0x0000FFFF;
     TIMER2_TAPR_R = 0;
     TIMER2_IMR_R = 0x00000001;
@@ -100,13 +84,16 @@ void Timer2AInit()
 volatile int value = 0;
 volatile int dummy = 0;
 
+/* 
+ * Define user tasks below as ProgA, ProgB, ProgC
+ */
+
 void ProgA()
 {
     while(1)
     {
         value = value + 1;
         int i,j;
-        //delay
         for( i = 0; i <120; i++)
             for(j = 0; j <1000;j++)
                 dummy = 3;
@@ -119,7 +106,6 @@ void ProgB()
     {
         value = value - 1;
         int i,j;
-        //delay
         for( i = 0; i <150; i++)
             for( j = 0; j <1000;j++)
                 dummy = 2;
@@ -150,9 +136,9 @@ int main(void) {
 
     // step 1: Activate clock for the port
     SYSCTL_RCGCGPIO_R |= 0x22; // enable clock for PORT F and PORTB
-    StackPtr = TCBs[0].StactPt;
+
     Timer2AInit();
-    bootstarping = 1;
+    bootstrapping = 1;
     while(1);
     //return 0;
 }
