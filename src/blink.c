318bcfc2ad5fe4005ad77645e92cf4247dd610f0
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2017-12-06 21:18:47 +0300

    Add real-time non-preemptive blink functions. YodaOS V0.1!

--- src/blink.c
+++ src/blink.c
@@ -7,15 +7,22 @@
 
 void blink1()
 {
-  return;
+
+	GPIO_PORTF_DATA_R = 0x2;
+	os_delay_ms(5);
+	GPIO_PORTF_DATA_R = 0x0;
 }
 
 void blink3()
 {
-  return;
+	GPIO_PORTC_DATA_R = 0x40;
+	os_delay_ms(5);
+	GPIO_PORTC_DATA_R = 0x00;
 }
 
 void blink5()
 {
-  return;
+	GPIO_PORTC_DATA_R ^= 0x80;
+	os_delay_ms(5);
+	GPIO_PORTC_DATA_R = 0x00;
 }
